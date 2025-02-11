ae81daa5f66bedd0d1283cba47e0ecfceec29828
nikhil <nikhilbutle08@gmail.com>
2025-02-11 08:52:20 +0530

    Thymleaf Introduciton and Iteration over a collection

---
+++ SpringBoot learning/src/main/java/com/bootjpa/example/jpaexample/Controller/ThymleafController.java
@@ -0,0 +1,33 @@
+package com.bootjpa.example.jpaexample.Controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Controller
+public class ThymleafController {
+
+    @RequestMapping(value = "/about",method = RequestMethod.GET)
+    public String about(Model model) {
+
+        model.addAttribute("name","Nikhil Butle");
+
+        model.addAttribute("date",new Date().toString());
+
+        return "about";
+    }
+
+    @GetMapping(value = "iterate-example")
+    public  String view(Model model){
+        List<String> names = List.of("Nikhil","Viraj","Sujal","Nachiket");
+        model.addAttribute("names", names);
+        return "iterate";
+    }
+}
